9e597841d44ec33bb089d342fe52481501390963
andreyosintsev <kintaro_oe@inbox.ru>
2025-11-25 14:02:08 +0400

    feat: popup design and displaying added

--- scripts/video.js
+++ scripts/video.js
@@ -1,18 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('video.js loaded');
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("video.js loaded");
 
-    const videos = document.querySelectorAll('.video__player');
+    const videos = document.querySelectorAll(".video__player");
+    console.log("videos found:", videos.length);
 
-    console.log('videos:', videos);
-
-    videos.forEach(video => {
-      video.addEventListener('click', () => {
-        if (video.paused) {
-          video.play();
-        } else {
-          video.pause();
-          video.currentTime = 0;
-        }
-      });
+    videos.forEach((video) => {
+        video.addEventListener("click", () => {
+            if (video.paused) {
+                playVideo(video);
+            } else {
+                pauseVideo(video);
+            }
+        });
     });
+
+    function playVideo(video) {
+        videos.forEach((video) => pauseVideo(video));
+        video.play();
+    }
+
+    function pauseVideo(video) {
+        video.pause();
+        video.currentTime = 0;
+    }
 });
